9d0d4961a7f31299bf8ea1219096c443a62789e8
namezbp1214 <3346461391@qq.com>
2025-05-23 20:56:05 +0800

    Site updated: 2025-05-23 20:56:02

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1748004192301";
+const VERSION = "1748004928569";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
